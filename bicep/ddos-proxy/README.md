183761b9a09fc0edd9dbbe6be3da7fdb3959932c
Daniel Larsen <dalars@microsoft.com>
2021-09-20 08:46:21 +1200

    Finish installation docs

--- bicep/ddos-proxy/README.md
+++ bicep/ddos-proxy/README.md
@@ -2,20 +2,44 @@
 
 ## Installation
 
+You will need
+
+* WSL2 (Ubuntu-20.04)
+* Latest AZ CLI installed in WSL2 environment
+
 ### Before you begin
 
-This sample uses an Azure DNS Zone, but does not include automation for creating the zone. Create a new zone or use an existing one. This sample will add an A record to the zone you specify.
+* Register a domain name and set the `dnsZoneName` parameter in `deploy.sh`
+* Reset the other parametes in `deploy.sh` as required
 
-* Create an Azure DNS Zone
-* Create a Key Vault and create a TLS Certificate
-
-
-
-## Notes
+### Run deploy script
 
 ```
-ddemo.fscale.nz
+./deploy.sh
 ```
+
+### Configure nameservers
+
+Script will output Name Servers. Configure these with the domain registrar.
+
+### Create a certificate request
+
+Script will output a CSR (certificate signing request). Copy into Certificate Authority's Certificate request form (Digicert is used in this example).
+
+![Request GeoTrust Cloud DV Certificate form](./docs/images/4-digicert-request.png)
+
+* Complete domain control validation (`TXT` record is easiest) and request the certificate
+* When certificate has been issued, download the `.CER` file ready to merge
+
+### Merge signed request
+
+ Upload the `.CER` file to the Key Vault (that has been created in the Resource Group) to merge the signed request.
+
+![Certificate operation - Merge signed request](./docs/images/7-merge-request.png)
+
+### Finish deployment
+
+Re-run `./deploy.sh` to deploy the remainder of the solution.
 
 ## References
 
--- bicep/ddos-proxy/infra/deploy.sh
+++ bicep/ddos-proxy/infra/deploy.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 set -e
 
-
+# Parameters - Change these to suit
 rgName='ddos-proxy-rg'
-adminUsername='localadmin'
-adminPassword='620e38a2-8c35-4055-a10a-8e48dd5b3e73'
 location='australiaeast'
-deploymentName='ddos-proxy-deployment'
-hostname='ddemo.fscale.nz'
-sshPublicKey=$(cat ~/.ssh/id_rsa.pub)
 vmssInstanceCount=3
 appGwyHostName='ddemo.fscale.nz'
 aRecordName='ddemo' 
 dnsZoneName='fscale.nz'
-forceUpdateTag=$(date +%N)
 dnsResourceGroupName='ddos-proxy-rg'
-certName='ddos-proxy-appgw-ssl-cert'
 
+# Constants
+adminUsername='localadmin'
+certName='ddos-proxy-appgw-ssl-cert'
+deploymentName='ddos-proxy-deployment'
+sshPublicKey=$(cat ~/.ssh/id_rsa.pub)
+forceUpdateTag=$(date +%N)
+
+
+# Create Resource Group
 az group create --name $rgName --location $location
 
+
+# Create storage account for VM web service assets
 az deployment group create \
     --name storageDeployment \
     --resource-group $rgName \
@@ -30,31 +34,34 @@ STORAGE_ACCOUNT_NAME=$(az deployment group show --name storageDeployment --resou
 CONTAINER_URI=$(az deployment group show --name storageDeployment --resource-group $rgName --query 'properties.outputs.storageContainerUri.value' -o tsv)
 SAS_TOKEN=$(az deployment group show --name storageDeployment --resource-group $rgName --query 'properties.outputs.storageAccountSasToken.value' -o tsv)
 
+# Upload VM web service assets
 az storage azcopy blob upload --container $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --source "../app/main" --recursive
 az storage azcopy blob upload --container $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --source "../scripts/install.sh" --recursive
 
-# DNS ZONE
-az deployment group create -n dnsZoneDeployment -g $rgName --template-file ./modules/dnsZone.bicep --parameters dnsZoneName=$dnsZoneName
+
+# Create DNS ZONE now so that TXT record can be created for CSR domain ownership verification
+az deployment group create -n dnsZoneDeployment -g $dnsResourceGroupName --template-file ./modules/dnsZone.bicep --parameters dnsZoneName=$dnsZoneName
 
 echo "NAME SERVERS =============="
-az network dns zone show -n $dnsZoneName -g $rgName --query 'nameServers'
+az network dns zone show -n $dnsZoneName -g $dnsResourceGroupName --query 'nameServers'
 echo "==========================="
-
 
 # Get current User Id 😻
 SIGNED_IN_USER_ID=$(az ad signed-in-user show --query 'objectId' -o tsv)
 
-# KEY VAULT
+
+# Deploy KEY VAULT now so that CSR can be created
 az deployment group create -n keyVaultDeployment -g $rgName --template-file ./modules/keyVault.bicep --parameters adminUserObjectId=$SIGNED_IN_USER_ID
 
 KEYVAULT_NAME=$(az deployment group show --name keyVaultDeployment --resource-group $rgName --query 'properties.outputs.keyVaultName.value' -o tsv)
 USER_IDENTITY_ID=$(az deployment group show --name keyVaultDeployment --resource-group $rgName --query 'properties.outputs.userIdentityId.value' -o tsv)
 
-# If Certificate does not exist in Key Vault
+# If Certificate does not exist in Key Vault, Create certificate request.
+# In this example, operator must manually merge the signed CSR before deploying the App GW
 if [[ $( az keyvault certificate list --vault-name $KEYVAULT_NAME --include-pending $true --query "length([? name=='${certName}'])" ) == 0 ]]; then
-    # Create certificate (Merge CSR manually later)
     export DNS_ZONE_NAME=$dnsZoneName
     export APPGW_HOST_NAME=$appGwyHostName
+    # Create a certificate policy from template
     envsubst < cert-policy-template.json > cert-policy.json
 
     echo "CSR ========================"
@@ -65,6 +72,7 @@ fi
 # Get certificate secret Id
 SSL_CERT_KV_SECRET_ID=$( az keyvault certificate show -n $certName --vault-name $KEYVAULT_NAME --query 'sid' -o tsv )
 
+# An uncompleted CSR returns no secret ID, so quit and wait for the operator to merge
 if [[ $SSL_CERT_KV_SECRET_ID = '' ]]; then
     echo "========================================="
     echo "Please Merge signed CSR before continuing"
@@ -72,13 +80,16 @@ if [[ $SSL_CERT_KV_SECRET_ID = '' ]]; then
     exit
 fi
 
+
+echo '==================================================='
+echo 'Enter a secure localadmin password for the VMSS VMs'
+
+# Deploy the solution
 az deployment group create \
     --name $deploymentName \
     --resource-group $rgName \
     --template-file ./main.bicep \
     --parameters adminUsername=$adminUsername \
-    --parameters adminPassword=$adminPassword \
-    --parameters appGwyHostName=$hostName \
     --parameters vmssInstanceCount=$vmssInstanceCount \
     --parameters vmssCustomScriptUri=$CONTAINER_URI \
     --parameters appGwyHostName=$appGwyHostName \
